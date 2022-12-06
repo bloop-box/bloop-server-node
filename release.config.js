029008e2a2d46a6aff80839ff4d54d992bbbe5ec
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-12-06 03:36:39 +0100

    build: remove automatic LICENSE update

--- release.config.js
+++ release.config.js
@@ -6,13 +6,12 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        'semantic-release-license',
         '@semantic-release/npm',
         '@semantic-release/github',
         [
             '@semantic-release/git',
             {
-                assets: ['CHANGELOG.md', 'LICENSE', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+                assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
                 message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]'
                     + '\n\n${nextRelease.notes}',
             },
