fcdce6918de9be3b9017766df5f64052a66486f6
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-12-10 12:24:14 +0100

    fix: user explicit type exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from './server';
-export * from './Processor';
+export type {StartServerResult, ServerOptions} from './server';
+export {startServer} from './server';
+
+export type {default as Processor} from './Processor';
+export {AudioFoundResult, AudioNotFoundResult, ThrottledUidResult, ValidUidResult, UnknownUidResult} from './Processor';
